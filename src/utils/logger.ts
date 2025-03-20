be90b1ce841051804e30b0373bea0dcb84afc0fb
Cristian Leonardo Fonseca <cristian.lfs@gmail.com>
2025-03-20 05:23:47 -0500

    Se añadió un comentario TODO en logger.ts para indicar la necesidad de eliminar una línea específica. Esto mejora la claridad del código y señala un posible ajuste futuro.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,7 @@
 import { Message } from 'discord.js';
 import fs from 'fs';
 import path from 'path';
+// TODO: quitar esta linea de alguna forma
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const chalk = require('chalk');
 
